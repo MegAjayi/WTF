2b03249f43aa3213040b4bd02681079a64b8daea
margret <megajayi93@gmail.com>
2025-09-05 12:24:20 +0100

    my assignment

---
+++ assingment.js
@@ -0,0 +1,12 @@
+let num1 = 3;
+let num2 = 6;
+let name = 'Margret';
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+
+let result = num1 + num2;
+
+console.log("Hello " + name + "," + "the result of num1 + num2 is " + result);
